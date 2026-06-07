452041ec75f327e32146015e328b2e79bfa0a50c
juliebe22 <juliabborgesc@gmail.com>
2026-06-07 00:17:17 -0300

    Add files via upload

---
+++ ContaView.java
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project-private xmlns="http://www.netbeans.org/ns/project-private/1">
+    <editor-bookmarks xmlns="http://www.netbeans.org/ns/editor-bookmarks/2" lastBookmarkId="1">
+        <file>
+            <url>src/br/com/teahub/model/Atendimento.java</url>
+            <bookmark id="1">
+                <name/>
+                <line>0</line>
+                <key/>
+            </bookmark>
+        </file>
+    </editor-bookmarks>
+    <open-files xmlns="http://www.netbeans.org/ns/projectui-open-files/2">
+        <group>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/config/ConexaoBanco.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/model/Clinica.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/model/Atendimento.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/dao/DAO.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/model/Paciente.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/model/Profissional.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/LoginView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/CadastroView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/MainView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/AtendimentoView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/CadastroPacienteView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/dao/ProfissionalDAO.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/dao/PacienteDAO.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/dao/AtendimentoDAO.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/teahub/TEAHub.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/dao/ClinicaDAO.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/view/ContaView.java</file>
+            <file>file:/C:/Users/Lia%20Rachel/Documents/NetBeansProjects/TEAhub/src/br/com/teahub/util/Sessao.java</file>
+        </group>
+    </open-files>
+</project-private>
