c39006ddf8d3e1f0f5ac43a0dfaa3de6702ba875
juliebe22 <juliabborgesc@gmail.com>
2026-06-07 00:38:10 -0300

    Fix SQL syntax by adding missing semicolon

--- dql/consultas.sql
+++ dql/consultas.sql
@@ -4,8 +4,8 @@ FROM atendimento a
 JOIN paciente p ON p.id_paciente = a.id_paciente
 JOIN profissional pr ON pr.id_profissional = a.id_profissional
 WHERE p.id_clinica = ?
-ORDER BY a.data_atendimento DESC
-:           
+ORDER BY a.data_atendimento DESC;
+
 SELECT a.id_atendimento, a.id_paciente, a.observacoes, a.data_atendimento, 
 p.nome_paciente, pr.id_profissional, pr.nome AS nome_profissional, pr.especialidade
 FROM atendimento a
